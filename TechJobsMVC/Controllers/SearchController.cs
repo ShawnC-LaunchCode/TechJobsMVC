571183977bc28ad51a9230882aae1aa66c1cb6be
Shawn C <scooter4356@gmail.com>
2020-07-17 18:08:38 -0500

    search working, display broken

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Models;
+using TechJobsMVC.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,5 +20,29 @@ namespace TechJobsMVC.Controllers
         }
 
         // TODO #3: Create an action method to process a search request and render the updated search view. 
+
+        [HttpPost]
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Job> output = new List<Job>();
+
+            if (searchTerm == "" || searchTerm == null) { output = JobData.FindAll(); }
+            else {output = JobData.FindByColumnAndValue(searchType, searchTerm);}
+
+            ViewBag.jobs = output;
+
+            ViewBag.columns = ListController.ColumnChoices;
+            
+
+
+
+
+
+
+
+            return Redirect("/search/index");
+
+        }
+
     }
 }
